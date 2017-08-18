6cccc46a5047e1cd7859156218617b182254ec69
Ashkan Moatamed <30676995+AshkanM96@users.noreply.github.com>
2017-08-17 21:01:54 -0400

    Update TMS.java

--- TMS.java
+++ TMS.java
@@ -2442,12 +2442,14 @@ public class TMS {
 	}
 
 	public static String lower(String s) {
-		if (s == null || s.length() <= 1) {
-			return s;
+		if (s == null) {
+			return null;
 		}
 
 		String lower = s.toLowerCase();
-		if (s.substring(1).equals(lower.substring(1))) {
+		if (s.length() <= 1) {
+			return lower;
+		} else if (s.substring(1).equals(lower.substring(1))) {
 			return lower; // matches [A-Z][a-z]*
 		} else if (s.equals(s.toUpperCase())) {
 			return lower; // matches [A-Z]+
