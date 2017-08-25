ee5551b409addc2b1cdbe9f968966c7dad38b962
Ashkan Moatamed <30676995+AshkanM96@users.noreply.github.com>
2017-08-24 21:48:22 -0400

    Update TMS.java

--- TMS.java
+++ TMS.java
@@ -2055,8 +2055,8 @@ public class TMS {
 				this.lineNumber++;
 			}
 			in.close(); // close upon success
-			// trim last extra '\n' from comments
-			this.offerComments(comments.subSequence(0, comments.length() - 1));
+			// trim last extra '\n' from comments if it exists
+			this.offerComments(comments.length() == 0 ? comments : comments.subSequence(0, comments.length() - 1));
 
 			this.isScanning = this.isConstructing = false;
 			this.cause = TMS.staticCause = null;
